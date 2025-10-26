eb64b815576ebaf6f4b79308573e17dc0abbed54
Arthur Gomes Soares <atkgomes@gmail.com>
2025-10-26 02:24:34 -0300

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,1 +1,57 @@
-
+tsParticles.load("tsparticles", {
+  fpsLimit: 60,
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 0.5,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: {
+        enable: false
+      }
+    },
+    move: {
+      enable: true,
+      speed: 0.3,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false
+      },
+      onclick: {
+        enable: false
+      },
+      resize: true
+    }
+  },
+  detectRetina: true
+});
